90d2d726f5e95101a068b522a454ff884b1d5a04
Rekhansh <rekhansh.singhwal@gmail.com>
2021-08-07 20:54:53 +0530

    New Version - HSL Mode - Slider Gradient - Hex Code

--- app/src/main/java/com/rekhansh/colorpickerexample/MainActivity.kt
+++ app/src/main/java/com/rekhansh/colorpickerexample/MainActivity.kt
@@ -3,30 +3,25 @@ package com.rekhansh.colorpickerexample
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.rekhansh.colorpicker.ColorPickerDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var selectedColor = Color.argb(255,0,0,0)
+    private var selectedColor = Color.argb(255, 0, 0, 255)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_color_picker.setOnClickListener{
-            ColorPickerDialog(selectedColor,object :ColorPickerDialog.ColorPickerDialogListener{
-                override fun onColorChange(red: Float, green: Float, blue: Float, alpha: Float) {
-                    selectedColor = Color.argb(alpha.toInt(),red.toInt(),green.toInt(), blue.toInt())
-                    colorView.setCardBackgroundColor(
-                        Color.argb(
-                            alpha.toInt(),
-                            red.toInt(),
-                            green.toInt(),
-                            blue.toInt()
-                        )
-                    )
+        colorView.setCardBackgroundColor(selectedColor)
+        colorView.setOnClickListener {
+            ColorPickerDialog(selectedColor, object : ColorPickerDialog.ColorPickerDialogListener {
+                override fun onSelectColor(color: Int) {
+                    selectedColor = color
+                    colorView.setCardBackgroundColor(color)
                 }
-            }).show(supportFragmentManager,"colorPicker")
+            }).show(supportFragmentManager, "colorPicker")
         }
     }
 }
